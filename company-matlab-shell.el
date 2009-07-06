da708adc623fdb4e0083be3392d95ded8aa5257c
zappo <zappo>
2009-07-06 20:36:56 +0000

    (company): Allow build when this require doesn't exist.

--- company-matlab-shell.el
+++ company-matlab-shell.el
@@ -15,7 +15,9 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-(require 'company)
+(condition-case nil
+    (require 'company)
+  (error nil))
 (eval-when-compile (require 'cl))
 (require 'matlab)
 
